fcd0d998137d8c934c7fc6f34440760b513daa06
Henrique Leite <hleite@uwo.ca>
2026-07-25 17:11:56 -0400

    Updated redeploy site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,8 @@ cd ~/team1-portfolio-henrique || exit
 # 2. Grab the latest changes from GitHub and force reset
 git fetch && git reset origin/main --hard
 
-# 3. Ensure dependencies are up to date
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# 3. Tear down the old containers
+docker compose -f docker-compose.prod.yml down
 
-# 4. Restart the systemd service to pick up changes
-systemctl restart myportfolio
+# 4. Rebuild and start fresh containers in the background
+docker compose -f docker-compose.prod.yml up -d --build
